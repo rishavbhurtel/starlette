6fbc5f6abdedd791aa933604adcf9c355f6dfeaa
rishavbhurtel <rishavmani.bhurtel@gmail.com>
2020-12-22 10:55:00 +0545

    reload solved

--- test.py
+++ test.py
@@ -36,4 +36,4 @@ routes = [
 app = Starlette(debug=True, routes=routes)
 app.mount('/static', StaticFiles(directory='statics'), name='static')
 if __name__ == "__main__":
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+    uvicorn.run('test:app', host='0.0.0.0', port=8000, reload=True)
