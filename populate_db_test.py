6f10c6dfa7019d8cb0038fdf1231338871d9c8fe
rishavbhurtel <rishavmani.bhurtel@gmail.com>
2020-12-23 08:59:45 +0545

    commit working

--- populate_db_test.py
+++ populate_db_test.py
@@ -7,7 +7,7 @@ DB_PATH = 'sqlite.db'
 
 def info():
     info = {
-        'id': fake.random_int(0,100),
+        'id': fake.random_int(0,10000),
         'name': fake.name(), 
         'address': fake.address()
     }
@@ -15,17 +15,19 @@ def info():
     return info
 
 def populate_info(infos):
+    #established db connection
     engine = create_engine(f'sqlite:///{DB_PATH}')
+    #create connection
     conn = engine.connect()
-
-    trans = conn.begin
+    #begin transaction
+    trans = conn.begin()
     for info in infos:
         conn.execute('INSERT INTO "infos"(id, '
                     'name, '
                     'address)'
                     f'VALUES(?,?,?)', [info[key] for key in info.keys()])
     trans.commit()
-
+    #close connection
     conn.close()
 
 if __name__ == "__main__":
